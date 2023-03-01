6ff3e2202c5baa4eed97775e40e35b48c7898078
kizwolak <ki.zwolak@gmail.com>
2023-03-01 13:28:41 -0300

    Independent component state sharing introduced

--- src/App.js
+++ src/App.js
@@ -3,25 +3,14 @@ import Shop from "./components/Shop";
 import Item from "./components/Item";
 import Basket from "./components/Basket";
 
-export default function App() {
-  const [basket, setBasket] = React.useState(0)
-  function handleItemClick() {
-    setBasket(prevValue => prevValue + 1);
-  }
-  const [list, setList] = React.useState([
-    {
-        name:'Chocolate Ship',
-        image:'./images/ship.jpg',
-        desc:'Don\'t let the perceived size fool you. This choco-ship can take you on voyages around the world, while at the same time making sure you never go hungry at sea. Do NOT eat the integral parts.'
-    }
-])
-const generateList = list.map(item => {
-  return <Item name={item.name} image={item.image} desc={item.desc} onClick={handleItemClick} key={item.name}/>
-})
+export default function App(props) {
+  const basket = props.basket
+  const list = props.list
+  const generateList = props.handleGenerate
+  const handleItemClick = props.handleItemClick
   return (
     <div className="app">
       <Shop basket={basket} handleClick={handleItemClick} handleGenerate={generateList} list={list}/>
-      <Basket basket={basket} />
     </div>
   )
 }
--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -2,13 +2,28 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import Basket from "./components/Basket";
+import Item from "./components/Item";
 
 export default function RouteSwitch() {
+    const [basket, setBasket] = React.useState(0)
+    function handleItemClick() {
+      setBasket(prevValue => prevValue + 1);
+    }
+    const [list, setList] = React.useState([
+      {
+          name:'Chocolate Ship',
+          image:'./images/ship.jpg',
+          desc:'Don\'t let the perceived size fool you. This choco-ship can take you on voyages around the world, while at the same time making sure you never go hungry at sea. Do NOT eat the integral parts.'
+      }
+  ])
+  const generateList = list.map(item => {
+    return <Item name={item.name} image={item.image} desc={item.desc} onClick={handleItemClick} key={item.name}/>
+  })
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<App />} />
-                <Route path="/basket" element={<Basket />} />
+                <Route path="/" element={<App basket={basket} handleClick={handleItemClick} list={list} handleGenerate={generateList}/>} />
+                <Route path="/basket" element={<Basket basket={basket}/>} />
             </Routes>
         </BrowserRouter>
     )
