654834feb0f5279f076a2bfcd7bca442aae02382
kizwolak <ki.zwolak@gmail.com>
2023-03-17 13:25:39 -0300

    Remove unused const

--- src/components/ItemDisplay.js
+++ src/components/ItemDisplay.js
@@ -3,28 +3,33 @@ import Navbar from "./Navbar";
 import { Link } from "react-router-dom";
 
 export default function ItemDisplay(props) {
-    const basket = props.basket
-    const [amount, setAmount] = React.useState(0);
-    return (
-        <>
-            <Navbar basket={basket} />
-            <Link to={{pathname:`/`}}>
-                    <p className="shop-link">← Go back to the main page</p>
-            </Link>
-            <div className="item-display">
-                <img src={props.item.image} alt='item' />
-                <div className="item-display-desc">
-                    <h1>{props.item.name}</h1>
-                    <p>{props.item.desc}</p>
-                    <div className="basket-interaction">
-                        <p className="itemPrice">{props.item.price} USD</p>
-                        <div className="item-amount">
-                        <input type="number" min='1' required id="amount-input" placeholder="0"/>
-                        <button onClick={props.onClick}>Add item</button>
-                    </div>
-                </div>
+  const basket = props.basket;
+  return (
+    <>
+      <Navbar basket={basket} />
+      <Link to={{ pathname: `/` }}>
+        <p className="shop-link">← Go back to the main page</p>
+      </Link>
+      <div className="item-display">
+        <img src={props.item.image} alt="item" />
+        <div className="item-display-desc">
+          <h1>{props.item.name}</h1>
+          <p>{props.item.desc}</p>
+          <div className="basket-interaction">
+            <p className="itemPrice">{props.item.price} USD</p>
+            <div className="item-amount">
+              <input
+                type="number"
+                min="1"
+                required
+                id="amount-input"
+                placeholder="0"
+              />
+              <button onClick={props.onClick}>Add item</button>
             </div>
-            </div>
-        </>
-    )
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
