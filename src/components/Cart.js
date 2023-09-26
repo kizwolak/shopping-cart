9215331d909b7fd96c07e854778e038369efd6fd
kizwolak <ki.zwolak@gmail.com>
2023-09-26 13:38:49 -0300

    Complete design revamp

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,12 +1,22 @@
 import React from "react";
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 export default function Cart(props) {
-    const basket = props.basket;
-    return (
-        <div className="cart">
-            <Link to={{pathname: '/basket', state: {basket}}} className="basket-img" basket={props.basket}><img src="./images/shopping-cart-outline-svgrepo-com.svg" alt="Shopping cart" /></Link>
-            <p data-testid='basket'>{props.basket}</p>
-        </div>
-    )
+  const basket = props.basket;
+  return (
+    <div className="cart">
+      <h5>shopping cart</h5>
+      <Link
+        to={{ pathname: "/basket", state: { basket } }}
+        className="basket-img"
+        basket={props.basket}
+      >
+        <img
+          src="./images/shopping-cart-outline-svgrepo-com.svg"
+          alt="Shopping cart"
+        />
+      </Link>
+      <p data-testid="basket">{props.basket}</p>
+    </div>
+  );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,10 +2,10 @@ import React from "react";
 import Cart from "./Cart";
 
 export default function Navbar(props) {
-    return (
-        <div className="navbar">
-            <p className="title">The Big Shop</p>
-            <Cart basket={props.basket}/>
-        </div>
-    )
+  return (
+    <div className="navbar">
+      <h1>The Luxurious</h1>
+      <Cart basket={props.basket} />
+    </div>
+  );
 }
